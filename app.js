3ee2acc3833b83c3cdc7b4ca60ea4a09111592d7
Vinicius Quirino <106350676+ViniciusQuirino@users.noreply.github.com>
2023-02-27 12:19:19 -0300

    finnished

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fileUpload = require('express-fileupload');
 const axios = require('axios');
 const mime = require('mime-types');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8005;
 
 const app = express();
 const server = http.createServer(app);
